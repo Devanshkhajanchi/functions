659b566b1026df6b66b659b30e5cd27075185a68
Devansh Khajanchi <khajd104@newschool.edu>
2025-03-31 17:58:48 -0400

    Added a bunch of Data and Variables

---
+++ assets/script.js
@@ -0,0 +1,66 @@
+let coffeeData = [{
+	'name': "Espresso", 
+	'color': "darkgoldenrod",
+	'height': 1
+}, {
+	'name': "Hot Water", 
+	'color': "skyblue",
+	'height': 1
+}, {
+	'name': "Milk Foam", 
+	'color': "bisque",
+	'height': 1
+}, {
+	'name': "Steamed Milk", 
+	'color': "ivory",
+	'height': 1
+}, {
+	'name': "Chocolate", 
+	'color': "chocolate",
+	'height': 1
+}, {
+	'name': "Ice Cream", 
+	'color': "lightyellow",
+	'height': 1
+}, {
+	'name': "Cream", 
+	'color': "mintcream",
+	'height': 1
+}, {
+	'name': "Drip Coffee", 
+	'color': "saddlebrown",
+	'height': 1
+}]
+
+let divOne = document.getElementById;
+let divTwo = document.getElementById;
+let divThree = document.getElementById;
+let divFour = document.getElementById;
+let divFive = document.getElementById;
+let divSix = document.getElementById;
+
+let divList = [divOne, divTwo, divThree, divFour, divFive, divSix];
+
+let counter = 0;
+let coffeeMix = [];
+
+let espressoButton = document.getElementById;
+let hotWaterButton = document.getElementById;
+let milkFoamButton = document.getElementById;
+let steamedMilkButton = document.getElementById;
+let chocolateButton = document.getElementById;
+let iceCreamButton = document.getElementById;
+let creamButton = document.getElementById;
+let dripCoffeeButton = document.getElementById;
+
+
+
+
+espressoButton.eventListener("click", ()=>{
+	counter += 1 
+	if(counter > 6){
+		alert("too much")
+	} else {
+
+	}
+})
