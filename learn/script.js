163f48cfc62a8e4eeb920e63c672343436f1281f
Devansh Khajanchi <khajd104@newschool.edu>
2025-04-20 18:26:34 -0400

    Added content and CSS for learn page

--- learn/script.js
+++
@@ -1,9 +0,0 @@
-let espressoReveal = document.getElementById("espresso-reveal")
-
-espressoReveal.addEventListener("mouseenter", ()=>{
-	espressoReveal.src = '../assets/single-espresso.png'
-})
-
-espressoReveal.addEventListener("mouseleave", ()=>{
-	espressoReveal.src = '../assets/mystery-drink.png'
-})
